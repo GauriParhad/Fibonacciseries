e0e2a72f689492b74eccfeee8f77645510eb43f4
Gaurip10 <thegauriparhad@gmail.com>
2021-08-09 08:23:16 +0530

    Fibonacci series

---
+++ src/Fibonacciseries.java
@@ -0,0 +1,25 @@
+public class Fibonacciseries {
+    static int fib(int n)
+    {
+        int f[] = new int[n+2];
+        int i;
+
+        f[0] = 0;
+        f[1] = 1;
+
+        for (i = 2; i <= n; i++)
+        {
+
+            f[i] = f[i-1] + f[i-2];
+        }
+
+        return f[n];
+    }
+
+    public static void main (String args[])
+    {
+        int n = 3;
+        System.out.println(fib(n));
+    }
+}
+
